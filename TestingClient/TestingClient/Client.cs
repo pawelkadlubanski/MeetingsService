3019f9c5acbc3c8fd3f029cc698b0937649b455a
pawel <pawel.kadlubanski@gmail.com>
2016-11-04 20:03:34 +0100

    Create testing Client

---
+++ TestingClient/TestingClient/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingClient
+{
+    [TestClass]
+    public class Client
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            WebMeetingService.WebMeetingService service = new WebMeetingService.WebMeetingService();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
